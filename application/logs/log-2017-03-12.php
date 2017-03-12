3a1d1ad7021e0bd180776c15372bd67e27fd4033
Vani Ardiansyah <Vani Ardiansyah>
2017-03-12 22:40:26 +0700

    Initial commit. CI downloaded

---
+++ application/logs/log-2017-03-12.php
@@ -0,0 +1,91 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2017-03-12 10:21:57 --> Config Class Initialized
+INFO - 2017-03-12 10:21:57 --> Hooks Class Initialized
+DEBUG - 2017-03-12 10:21:57 --> UTF-8 Support Enabled
+INFO - 2017-03-12 10:21:57 --> Utf8 Class Initialized
+INFO - 2017-03-12 10:21:57 --> URI Class Initialized
+DEBUG - 2017-03-12 10:21:57 --> No URI present. Default controller set.
+INFO - 2017-03-12 10:21:57 --> Router Class Initialized
+INFO - 2017-03-12 10:21:57 --> Output Class Initialized
+INFO - 2017-03-12 10:21:57 --> Security Class Initialized
+DEBUG - 2017-03-12 10:21:57 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-03-12 10:21:57 --> Input Class Initialized
+INFO - 2017-03-12 10:21:57 --> Language Class Initialized
+INFO - 2017-03-12 10:21:57 --> Loader Class Initialized
+INFO - 2017-03-12 10:21:57 --> Controller Class Initialized
+INFO - 2017-03-12 10:21:57 --> File loaded: C:\xampp\htdocs\application\views\welcome_message.php
+INFO - 2017-03-12 10:21:57 --> Final output sent to browser
+DEBUG - 2017-03-12 10:21:57 --> Total execution time: 0.3947
+ERROR - 2017-03-12 16:13:18 --> 404 Page Not Found: Faviconico/index
+ERROR - 2017-03-12 16:13:18 --> 404 Page Not Found: Faviconico/index
+INFO - 2017-03-12 16:13:23 --> Config Class Initialized
+INFO - 2017-03-12 16:13:23 --> Hooks Class Initialized
+DEBUG - 2017-03-12 16:13:23 --> UTF-8 Support Enabled
+INFO - 2017-03-12 16:13:23 --> Utf8 Class Initialized
+INFO - 2017-03-12 16:13:23 --> URI Class Initialized
+DEBUG - 2017-03-12 16:13:23 --> No URI present. Default controller set.
+INFO - 2017-03-12 16:13:23 --> Router Class Initialized
+INFO - 2017-03-12 16:13:23 --> Output Class Initialized
+INFO - 2017-03-12 16:13:23 --> Security Class Initialized
+DEBUG - 2017-03-12 16:13:24 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-03-12 16:13:24 --> Input Class Initialized
+INFO - 2017-03-12 16:13:24 --> Language Class Initialized
+INFO - 2017-03-12 16:13:24 --> Loader Class Initialized
+INFO - 2017-03-12 16:13:24 --> Controller Class Initialized
+INFO - 2017-03-12 16:13:24 --> File loaded: C:\xampp\htdocs\application\views\welcome_message.php
+INFO - 2017-03-12 16:13:24 --> Final output sent to browser
+DEBUG - 2017-03-12 16:13:24 --> Total execution time: 0.4094
+INFO - 2017-03-12 16:13:24 --> Config Class Initialized
+INFO - 2017-03-12 16:13:24 --> Hooks Class Initialized
+DEBUG - 2017-03-12 16:13:24 --> UTF-8 Support Enabled
+INFO - 2017-03-12 16:13:24 --> Utf8 Class Initialized
+INFO - 2017-03-12 16:13:24 --> URI Class Initialized
+INFO - 2017-03-12 16:13:24 --> Router Class Initialized
+INFO - 2017-03-12 16:13:24 --> Output Class Initialized
+INFO - 2017-03-12 16:13:24 --> Security Class Initialized
+DEBUG - 2017-03-12 16:13:24 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-03-12 16:13:24 --> Input Class Initialized
+INFO - 2017-03-12 16:13:24 --> Language Class Initialized
+ERROR - 2017-03-12 16:13:24 --> 404 Page Not Found: Faviconico/index
+INFO - 2017-03-12 16:13:24 --> Config Class Initialized
+INFO - 2017-03-12 16:13:24 --> Hooks Class Initialized
+DEBUG - 2017-03-12 16:13:24 --> UTF-8 Support Enabled
+INFO - 2017-03-12 16:13:25 --> Utf8 Class Initialized
+INFO - 2017-03-12 16:13:25 --> URI Class Initialized
+INFO - 2017-03-12 16:13:25 --> Router Class Initialized
+INFO - 2017-03-12 16:13:25 --> Output Class Initialized
+INFO - 2017-03-12 16:13:25 --> Security Class Initialized
+DEBUG - 2017-03-12 16:13:25 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-03-12 16:13:25 --> Input Class Initialized
+INFO - 2017-03-12 16:13:25 --> Language Class Initialized
+ERROR - 2017-03-12 16:13:25 --> 404 Page Not Found: Faviconico/index
+INFO - 2017-03-12 16:20:03 --> Config Class Initialized
+INFO - 2017-03-12 16:20:03 --> Hooks Class Initialized
+DEBUG - 2017-03-12 16:20:03 --> UTF-8 Support Enabled
+INFO - 2017-03-12 16:20:03 --> Utf8 Class Initialized
+INFO - 2017-03-12 16:20:03 --> URI Class Initialized
+DEBUG - 2017-03-12 16:20:03 --> No URI present. Default controller set.
+INFO - 2017-03-12 16:20:03 --> Router Class Initialized
+INFO - 2017-03-12 16:20:03 --> Output Class Initialized
+INFO - 2017-03-12 16:20:03 --> Security Class Initialized
+DEBUG - 2017-03-12 16:20:03 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-03-12 16:20:03 --> Input Class Initialized
+INFO - 2017-03-12 16:20:03 --> Language Class Initialized
+INFO - 2017-03-12 16:20:04 --> Loader Class Initialized
+INFO - 2017-03-12 16:20:04 --> Controller Class Initialized
+INFO - 2017-03-12 16:20:04 --> File loaded: C:\xampp\htdocs\ci_bestpractice\application\views\welcome_message.php
+INFO - 2017-03-12 16:20:04 --> Final output sent to browser
+DEBUG - 2017-03-12 16:20:04 --> Total execution time: 0.5239
+INFO - 2017-03-12 16:20:21 --> Config Class Initialized
+INFO - 2017-03-12 16:20:21 --> Hooks Class Initialized
+DEBUG - 2017-03-12 16:20:21 --> UTF-8 Support Enabled
+INFO - 2017-03-12 16:20:21 --> Utf8 Class Initialized
+INFO - 2017-03-12 16:20:21 --> URI Class Initialized
+INFO - 2017-03-12 16:20:21 --> Router Class Initialized
+INFO - 2017-03-12 16:20:21 --> Output Class Initialized
+INFO - 2017-03-12 16:20:21 --> Security Class Initialized
+DEBUG - 2017-03-12 16:20:21 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-03-12 16:20:21 --> Input Class Initialized
+INFO - 2017-03-12 16:20:21 --> Language Class Initialized
+ERROR - 2017-03-12 16:20:21 --> 404 Page Not Found: Application/index
